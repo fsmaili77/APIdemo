2166088d608672601d58696c3cee89a005d75134
Fehmir <fehmir@gmail.com>
2023-09-10 14:58:56 +0200

    Sample project for creation and calling API in Asp.Net Core

---
+++ Controllers/ProduitsController.cs
@@ -0,0 +1,42 @@
+﻿using ApiDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProduitsController : Controller
+    {
+        private static List<Produit> produits = new()
+        {
+                new Produit { Id = 1, Nom = "Livre", Prix = 10 },
+                new Produit { Id = 2, Nom = "Stylo", Prix = 2 },
+                new Produit { Id = 3, Nom = "Cahier", Prix = 3 }
+            };
+        
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(produits);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var produit = produits.FirstOrDefault(x => x.Id == id);
+            if (produit == null)
+            {
+                return NotFound($"Le produit avec id: {id} n'existe pas");
+            }
+            return Ok(produit);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Produit produit)
+        {
+            produit.Id = produits.Max(p => p.Id) + 1;
+            produits.Add(produit);
+            return Ok(produits);
+        }
+    }
+}
---
+++ Models/Produit.cs
@@ -0,0 +1,14 @@
+﻿namespace ApiDemo.Models
+{
+    public class Produit
+    {
+        // Identifiant du produit
+        public int Id { get; set; }
+
+        // Nom du produit
+        public string? Nom { get; set; }
+
+        // Prix du produit
+        public decimal Prix { get; set; }
+    }
+}
